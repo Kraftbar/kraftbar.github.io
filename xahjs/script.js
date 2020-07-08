df0e924b9147d50c88bea3248b408f22b126a1b2
Gaute <gautenybo@gmail.com>
2020-07-08 17:31:37 +0200

    Update script.js

--- xahjs/script.js
+++ xahjs/script.js
@@ -1,6 +1,6 @@
 // TODO
 //   - lower / capital
-//   - create function parse more than one correct answer
+//   - create function parse more than one correct answer, ignore "()" and select on ","
 
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
