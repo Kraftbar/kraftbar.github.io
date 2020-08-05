94c4416e1f266a5a1727fb64e40258e860eb09e7
Kraftbar <Gautenybo@gmail.com>
2020-08-05 10:28:40 +0200

    update

--- hiddenFIles/trans_working/wrap-to-table.sh
+++ hiddenFIles/trans_working/wrap-to-table.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+
+echo -n "" > output
+filename='input'
+n=1
+while read line; do
+./gt.sh line >> output
+n=$((n+1))
+done < $filename
+
+
+
 cat output | awk -v FS='\t' '
     BEGIN{print "<table>"} 
     {printf("<tr><td>%s</td><td>%s</td><td>%s</td></tr>\n",$1,$2,$3)}
